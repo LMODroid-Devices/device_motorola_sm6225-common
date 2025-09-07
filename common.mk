0a9ad112b7c767edd15ffc438819a92217e10013
LMO GerritBot <gerrit@libremobileos.com>
2025-09-07 12:41:54 +0000

    Adapt to LMODroid by device-add script

--- common.mk
+++ common.mk
@@ -236,7 +236,7 @@ PRODUCT_PACKAGES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-lmodroid
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
